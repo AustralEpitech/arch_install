76d980823e4decdb68dd9ffb9586361d1291ae30
AngeD <ange.duhayon@epitech.eu>
2021-06-16 21:44:14 +0200

    feat: updated awesome config

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -47,14 +47,18 @@ end
 
 -- {{{ Variable definitions
 config_dir = gears.filesystem.get_configuration_dir()
+wallpaper_dir = config_dir .. "wallpapers/"
+
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+
+mytags = {"TTY", "WEB", "DEV", "FUN", "VM", "CHAT", "MED", "GFX", "ETC"}
 
 terminal = "alacritty"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 navigator = "brave"
-wallpaper = config_dir .. "wallpaper.png"
-lock = "i3lock -fti" .. config_dir .. "lock.png"
+wallpaper = {"wow2.png", "wow1.png", "wow3.png"}
+lock = "i3lock -fti" .. wallpaper_dir .. "lock.png"
 screenshot = "flameshot full -c"
 screenshot_gui = "flameshot gui"
 
@@ -80,29 +84,10 @@ awful.layout.layouts = {
 }
 -- }}}
 
--- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-    {"hotkeys",function() hotkeys_popup.show_help(nil, awful.screen.focused()) end},
-    {"manual", terminal .. " -e man awesome"},
-    {"edit config", editor_cmd .. " " .. awesome.conffile},
-    {"restart", awesome.restart},
-    {"quit", function() awesome.quit() end},
- }
-
-mymainmenu = awful.menu({
-    items = {
-        {"awesome", myawesomemenu, beautiful.awesome_icon},
-        {"open terminal", terminal}
-    }
-})
-
 -- Keyboard map indicator and switcher
 mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 local taglist_buttons = gears.table.join(
@@ -178,14 +163,15 @@ local tasklist_buttons = gears.table.join(
 )
 
 local function set_wallpaper(s)
-    -- Wallpaper
     if beautiful.wallpaper then
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
+        gears.wallpaper.maximized(wallpaper_dir .. wallpaper[s.index], s, true)
     end
+end
+
+local function set_clock(s)
+    s.mytextclock = wibox.widget.textclock()
+    s.month_calendar = awful.widget.calendar_popup.month({screen = s})
+    s.month_calendar:attach(s.mytextclock)
 end
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
@@ -193,11 +179,12 @@ screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(
     function(s)
-        -- Wallpaper
+        -- Wallpaper and clock
         set_wallpaper(s)
+        set_clock(s)
 
         -- Each screen has its own tag table.
-        awful.tag({"TTY", "WEB", "DEV", "FUN", "VM", "CHAT", "MED", "GFX", "ETC"}, s, awful.layout.layouts[1])
+        awful.tag(mytags, s, awful.layout.layouts[1])
 
         -- Create an imagebox widget which will contain an icon indicating which layout we're using.
         -- We need one layoutbox per screen.
@@ -250,7 +237,6 @@ awful.screen.connect_for_each_screen(
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                mylauncher,
                 s.mytaglist,
             },
             s.mytasklist, -- Middle widget
@@ -258,7 +244,7 @@ awful.screen.connect_for_each_screen(
                 layout = wibox.layout.fixed.horizontal,
                 mykeyboardlayout,
                 wibox.widget.systray(),
-                mytextclock,
+                s.mytextclock,
                 s.mylayoutbox,
             },
         }
@@ -266,11 +252,16 @@ awful.screen.connect_for_each_screen(
 )
 -- }}}
 
--- {{{ mouse bindings
+-- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({}, 3, function () mymainmenu:toggle() end),
-    awful.button({}, 4, awful.tag.viewnext),
-    awful.button({}, 5, awful.tag.viewprev)
+    awful.button(
+        {}, 4,
+        awful.tag.viewnext
+    ),
+    awful.button(
+        {}, 5,
+        awful.tag.viewprev
+    )
 ))
 -- }}}
 
@@ -337,6 +328,20 @@ globalkeys = gears.table.join(
         {description = "swap with previous client by index", group = "client"}
     ),
     awful.key(
+        {modkey, "Control"}, "j",
+        function()
+            awful.screen.focus_relative(1)
+        end,
+        {description = "focus the next screen", group = "screen"}
+    ),
+    awful.key(
+        {modkey, "Control"}, "k",
+        function()
+            awful.screen.focus_relative(-1)
+        end,
+        {description = "focus the previous screen", group = "screen"}
+    ),
+    awful.key(
         {modkey}, "u",
         awful.client.urgent.jumpto,
         {description = "jump to urgent client", group = "client"}
@@ -361,21 +366,6 @@ globalkeys = gears.table.join(
             end
         end,
         {description = "restore minimized", group = "client"}
-    ),
-
-    awful.key(
-        {modkey, "Control"}, "j",
-        function()
-            awful.screen.focus_relative(1)
-        end,
-        {description = "focus the next screen", group = "screen"}
-    ),
-    awful.key(
-        {modkey, "Control"}, "k",
-        function()
-            awful.screen.focus_relative(-1)
-        end,
-        {description = "focus the previous screen", group = "screen"}
     ),
 
     -- Standard program
@@ -436,34 +426,6 @@ globalkeys = gears.table.join(
         end,
         {description = "decrease master width factor", group = "layout"}
     ),
-    -- awful.key(
-    --     {modkey, "Shift"}, "h",
-    --     function()
-    --         awful.tag.incnmaster(1, nil, true)
-    --     end,
-    --     {description = "increase the number of master clients", group = "layout"}
-    -- ),
-    -- awful.key(
-    --     {modkey, "Shift"}, "l",
-    --     function()
-    --         awful.tag.incnmaster(-1, nil, true)
-    --     end,
-    --     {description = "decrease the number of master clients", group = "layout"}
-    -- ),
-    -- awful.key(
-    --     {modkey, "Control"}, "h",
-    --     function()
-    --         awful.tag.incncol(1, nil, true)
-    --     end,
-    --     {description = "increase the number of columns", group = "layout"}
-    -- ),
-    -- awful.key(
-    --     {modkey, "Control"}, "l",
-    --     function()
-    --         awful.tag.incncol(-1, nil, true)
-    --     end,
-    --     {description = "decrease the number of columns", group = "layout"}
-    -- ),
     awful.key(
         {modkey}, "space",
         function()
@@ -480,7 +442,6 @@ globalkeys = gears.table.join(
     ),
 
     -- Hotkeys
-
     awful.key(
         {}, "XF86AudioRaiseVolume",
         function()
@@ -641,7 +602,7 @@ awful.rules.rules = {
             keys = clientkeys,
             buttons = clientbuttons,
             screen = awful.screen.preferred,
-            placement = awful.placement.no_overlap+awful.placement.no_offscreen
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen
         }
     },
 
@@ -680,79 +641,35 @@ awful.rules.rules = {
         properties = {floating = true}
     },
 
-    -- Add titlebars to normal clients and dialogs
     {
-        rule_any = {
-            type = {"normal", "dialog"}
-        },
-        properties = {titlebars_enabled = true}
-    },
-
-    {
-        rule_any = {
-            class = {"Brave-browser"}
-        },
-        properties = {
-            tag = root.tags()[2],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"Brave-browser"}},
+        properties = {tag = mytags[2], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"code-oss"}
-        },
-        properties = {
-            tag = root.tags()[3],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"code-oss"}},
+        properties = {tag = mytags[3], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"Steam", "lutris"}
-        },
-        properties = {
-            tag = root.tags()[4],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"Steam", "lutris"}},
+        properties = {tag = mytags[4], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"Virt-manager"}
-        },
-        properties = {
-            tag = root.tags()[5],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"Virt-manager"}},
+        properties = {tag = mytags[5], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"discord"}
-        },
-        properties = {
-            tag = root.tags()[6],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"discord"}},
+        properties = {tag = mytags[6], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"vlc"}
-        },
-        properties = {
-            tag = root.tags()[7],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"vlc"}},
+        properties = {tag = mytags[7], switch_to_tags = true}
     },
     {
-        rule_any = {
-            class = {"Gimp"}
-        },
-        properties = {
-            tag = root.tags()[8],
-            switch_to_tags = true,
-        }
+        rule_any = {class = {"Gimp"}},
+        properties = {tag = mytags[8], switch_to_tags = true}
     }
 }
-
 -- }}}
 
 -- {{{ Signals
@@ -765,9 +682,9 @@ client.connect_signal(
         -- if not awesome.startup then awful.client.setslave(c) end
 
         if awesome.startup and
-           not c.size_hints.user_position and
-           not c.size_hints.program_position then
-                -- Prevent clients from being unreachable after screen count changes.
+        not c.size_hints.user_position and
+        not c.size_hints.program_position then
+            -- Prevent clients from being unreachable after screen count changes.
             awful.placement.no_offscreen(c)
         end
     end
@@ -785,6 +702,7 @@ client.connect_signal(
         c.border_color = beautiful.border_normal
     end
 )
+-- }}}
 
 apps = {
     terminal,
@@ -800,5 +718,5 @@ apps = {
 }
 
 for _, app in ipairs(apps) do
-    awful.spawn(app)
+    awful.spawn.once(app)
 end

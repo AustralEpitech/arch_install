c21cadf292b9b2247d3d1786585e9b59e190a928
AngeD <ange.duhayon@epitech.eu>
2021-07-28 21:17:59 +0200

    fix: syntax errors

--- base_install.sh
+++ base_install.sh
@@ -63,13 +63,15 @@ TECH_PACKAGES=(
 
 ask() {
     while true; do
-        read -p "${BOLD}:: $1 [Y/n] ${NORMAL}" ANS
+        read -rp "${BOLD}:: $1 [Y/n] ${NORMAL}" ANS
         case "${ANS^^}" in
             '' | 'Y' | 'YES')
                 return 1
+                ;;
             'N' | 'NO')
                 return 0
-        echo
+                ;;
+        esac
     done
 }
 
@@ -86,7 +88,7 @@ check_system() {
 }
 
 copy_system_config() {
-    "$CP" -r "$CONFIG_DIR"/etc /etc
+    $CP -r "$CONFIG_DIR"/etc /etc
 }
 
 set_timezone() {
@@ -96,7 +98,7 @@ set_timezone() {
 }
 
 set_locale() {
-    "$SED" 's/^#en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/; s/^#fr_FR.UTF-8 UTF-8$/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+    $SED 's/^#en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/; s/^#fr_FR.UTF-8 UTF-8$/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
     locale-gen
     echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 }
@@ -109,7 +111,7 @@ set_hostname() {
 download_packages() {
     pacman "$PAC_OPT"yyu reflector
     systemctl start reflector
-    pacman "$PAC_OPT" "${PACKAGES[@]}"
+    pacman "$PAC_OPT" "${PACKAGES[*]}"
 }
 
 create_user() {
@@ -118,26 +120,26 @@ create_user() {
 
     useradd -mG wheel "$USERNAME"
 
-    echo "${BOLD}$USERNAME passwd${NORMAL}"
+    echo -e "${BOLD}$USERNAME passwd${NORMAL}"
     passwd "$USERNAME"
-    "$SED" 's/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/' /etc/sudoers
+    $SED 's/^# %wheel ALL=(ALL) ALL$/%wheel ALL=(ALL) ALL/' /etc/sudoers
 }
 
 download_special_packages() {
-    "$SU" "$PWD/install_paru.sh && paru $PAC_OPT ${AUR_PACKAGES[@]}"
-    ask 'Do you want tech packages?' && "$SU" "paru $PAC_OPT ${TECH_PACKAGES[@]}"
+    $SU "$PWD/install_paru.sh && paru $PAC_OPT ${AUR_PACKAGES[*]}"
+    ask 'Do you want tech packages?' && $SU "paru $PAC_OPT ${TECH_PACKAGES[*]}"
 }
 
 set_bootloader() {
     bootctl install
-    "$CP" /usr/share/systemd/bootctl/arch.conf "$ENTRIES"
+    $CP /usr/share/systemd/bootctl/arch.conf "$ENTRIES"
     echo -e 'default arch.conf\ntimeout 3\neditor  no' > /boot/loader/loader.conf
 
-    "$SED" '/^$/d; /^#/d;' "$ENTRIES"arch.conf
-    "$SED" "s/^options root=PARTUUID=XXXX rootfstype=XXXX add_efi_memmap$/options root=/dev/$DISK/" "$ENTRIES"arch.conf
+    $SED '/^$/d; /^#/d;' "$ENTRIES"arch.conf
+    $SED "s/^options root=PARTUUID=XXXX rootfstype=XXXX add_efi_memmap$/options root=/dev/$ROOT_DISK/" "$ENTRIES"arch.conf
 
-    "$CP" "$ENTRIES"arch.conf "$ENTRIES"arch-fallback.conf
-    "$SED" 's/Arch Linux$/Arch Linux (fallback initramfs)/; s/initramfs-linux.img$/initramfs-linux-fallback.img/' "$ENTRIES"arch-fallback.conf
+    $CP "$ENTRIES"arch.conf "$ENTRIES"arch-fallback.conf
+    $SED 's/Arch Linux$/Arch Linux (fallback initramfs)/; s/initramfs-linux.img$/initramfs-linux-fallback.img/' "$ENTRIES"arch-fallback.conf
 
     echo 'initrd  /intel-ucode.img' >> "$ENTRIES"arch.conf
 }
@@ -148,16 +150,15 @@ enable_network() {
 
 configure_graphics() {
     nvidia-xconfig
-    "$SED" 's/^MODULES=()$/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+    $SED 's/^MODULES=()$/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
 }
 
 copy_dotfiles() {
-    ask 'Copy dotfiles?' && "$CP" -r "$CONFIG_DIR"/.* /home/"$USERNAME"
+    ask 'Copy dotfiles?' && $CP -r "$CONFIG_DIR"/.* /home/"$USERNAME"
 
-    # Oh My Zsh
-    ask 'Install OMZ?' && "$SU" 'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    ask 'Install OMZ?' && $SU "sh -c '$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'"
 
-    ask 'Copy binaries?' && "$CP" "$CONFIG_DIR"/bin /home/"$USERNAME"
+    ask 'Copy binaries?' && $CP "$CONFIG_DIR"/bin /home/"$USERNAME"
 }
 
 main() {
@@ -173,7 +174,7 @@ main() {
     enable_network
     configure_graphics
     copy_dotfiles
-    echo "${BOLD}${GREEN}DONE. Ctrl+D, umount -R /mnt and reboot to exit${NORMAL}"
+    echo -e "${BOLD}${GREEN}DONE. Ctrl+D, umount -R /mnt and reboot to exit${NORMAL}"
 }
 
 set -e
